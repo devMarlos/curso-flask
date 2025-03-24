d5394df5d653dd737b1b0090980d3520fc11ec48
devMarlos <smarlosilan@gmail.com>
2025-03-23 21:53:48 -0400

    Lista de fruta e utilização de rotas

---
+++ lista_frutas/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+frutas = []
+titulo = 'Lista de Frutas'
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo=titulo, frutas=frutas)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['fruta']:
+        frutas.append(request.form['fruta'])
+    
+    return redirect(url_for('index'))
+
+app.run(host='0.0.0.0', port=8080, debug=True)
